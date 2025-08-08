94805de97dbf7b2a714a47449e5fb82f1a92ebb9
denyschr <denprogman99@gmail.com>
2025-08-08 19:53:15 +0300

    feat: implement generate button

--- src/app/password-generator/password-generator.ts
+++ src/app/password-generator/password-generator.ts
@@ -7,7 +7,6 @@ const DIGITS = '0123456789';
 const SYMBOLS = '!@#$%^&*()_+-=[]{}|\\:;"\'<>,.?/';
 const DEFAULT_PASSWORD_LENGTH = 12;
 
-
 @Component({
   selector: 'app-password-generator',
   templateUrl: './password-generator.html',
@@ -21,4 +20,37 @@ export class PasswordGenerator {
   protected readonly includesUppercase = signal(false);
   protected readonly includesDigits = signal(false);
   protected readonly includesSymbols = signal(false);
+
+  protected generate(): void {
+    const charSets: string [] = [];
+    if (this.includesLowercase()) {
+      charSets.push(LOWERCASE_LETTERS);
+    }
+    if (this.includesUppercase()) {
+      charSets.push(UPPERCASE_LETTERS);
+    }
+    if (this.includesDigits()) {
+      charSets.push(DIGITS);
+    }
+    if (this.includesSymbols()) {
+      charSets.push(SYMBOLS);
+    }
+
+    const allChars = charSets.join('');
+    const remainingLength = this.passwordLength() - charSets.length;
+
+    const getRandomChar = () => allChars[this.getRandomInt(allChars.length)];
+
+    const randomChars = charSets
+      .map((charSet) => charSet[this.getRandomInt(charSet.length)])
+      .concat(Array.from({ length: remainingLength }, getRandomChar))
+      .sort(() => 0.5 - Math.random())
+      .join('');
+
+    this.password.set(randomChars);
+  }
+
+  private getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
 }

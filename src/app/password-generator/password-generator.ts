fb2c255bd5ab55247b6e92f300b60992ff40129c
denyschr <denprogman99@gmail.com>
2025-08-08 17:15:08 +0300

    feat: synchronize password-length input with output

--- src/app/password-generator/password-generator.ts
+++ src/app/password-generator/password-generator.ts
@@ -1,8 +1,16 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core'
+import {FormsModule} from '@angular/forms'
+
+const DEFAULT_PASSWORD_LENGTH = 12;
 
 @Component({
   selector: 'app-password-generator',
   templateUrl: './password-generator.html',
+  imports: [
+    FormsModule
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PasswordGenerator {}
+export class PasswordGenerator {
+  protected readonly passwordLength = signal(DEFAULT_PASSWORD_LENGTH);
+}

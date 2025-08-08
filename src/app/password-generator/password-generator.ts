ade6e9782c022d565937779ad21971fb1ad203d5
denyschr <denprogman99@gmail.com>
2025-08-08 17:54:54 +0300

    feat: synchronize checkboxes and password output

--- src/app/password-generator/password-generator.ts
+++ src/app/password-generator/password-generator.ts
@@ -1,16 +1,24 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core'
 import {FormsModule} from '@angular/forms'
 
+const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+const UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const DIGITS = '0123456789';
+const SYMBOLS = '!@#$%^&*()_+-=[]{}|\\:;"\'<>,.?/';
 const DEFAULT_PASSWORD_LENGTH = 12;
+
 
 @Component({
   selector: 'app-password-generator',
   templateUrl: './password-generator.html',
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PasswordGenerator {
+  protected readonly password = signal('');
   protected readonly passwordLength = signal(DEFAULT_PASSWORD_LENGTH);
+  protected readonly includesLowercase = signal(true);
+  protected readonly includesUppercase = signal(false);
+  protected readonly includesDigits = signal(false);
+  protected readonly includesSymbols = signal(false);
 }
